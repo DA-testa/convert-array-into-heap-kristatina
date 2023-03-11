3168451e85091486dc005ebe228a1805a6709477
Kristīne Berķe <125013804+kristatina@users.noreply.github.com>
2023-03-11 23:04:11 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -5,24 +5,22 @@ def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-    x = len(data)
     heapq.heapify(data)
     swapCount = 0
-    for i in range(x):
+    for i in range(len(data)):
         swaps.append(heapq.heappop(data))
         swapCount += 1
     return swaps, swapCount
 
-def main():
+def main(data):
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-    x = len(data)
-    for i in range(x):
-        if 2*i+1 < x and data[2*i+1] < data[i]:
+    for i in range(len(data)):
+        if 2*i+1 < len(data) and data[2*i+1] < data[i]:
             return False
-        if 2*i+2 < 2 and data[2*i+2] < data[i]:
+        if 2*i+2 < len(data) and data[2*i+2] < data[i]:
             return False
     return True
 
